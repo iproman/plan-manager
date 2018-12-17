847cd722a46b831d5fa1dbd89362f4419e5a39d8
iproman <itechman@yandex.ru>
2018-12-18 00:45:31 +0500

    Add radioList for status

--- views/task/_form.php
+++ views/task/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use app\models\Task;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Task */
@@ -11,14 +12,35 @@ use yii\widgets\ActiveForm;
 <div class="task-form">
 
     <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'content')->textarea(['maxlength' => true, 'rows' => 10]) ?>
-
+    <div class="row">
+        <div class="col-lg-12">
+            <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <?= $form->field($model, 'content')->textarea(['maxlength' => true, 'rows' => 10]) ?>
+        </div>
+    </div>
     <div class="row">
         <div class="col-lg-3">
-            <?= $form->field($model, 'status')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'status')->radioList(
+                Task::getStatusLabels(),
+                [
+                    'class' => 'btn-group',
+                    'data-toggle' => 'buttons',
+                    'unselect' => null,
+                    'item' => function ($index, $label, $name, $checked, $value) {
+                        return Html::tag(
+                            'label',
+                            Html::radio($name, $checked, ['value' => $value]) . $label,
+                            [
+                                'class' => 'btn btn-' . Task::getStatusCss()[$value] . ($checked ? ' active' : ''),
+                            ]
+                        );
+                    },
+                ]
+            ) ?>
         </div>
         <div class="col-lg-3">
             <?= $form->field($model, 'branch')->textInput(['maxlength' => true]) ?>
@@ -29,7 +51,7 @@ use yii\widgets\ActiveForm;
             <?php endif; ?>
         </div>
     </div>
-
+    <hr>
     <div class="form-group">
         <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
     </div>
