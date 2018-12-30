f6717e78877920b3dcd6d4e398190ae03f195f20
iproman <itechman@yandex.ru>
2018-12-30 19:51:11 +0500

    Add asset for SBAdmin2

---
+++ assets/themes/SBAdmin2Asset.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: iproman
+ * Date: 30.12.2018
+ * Time: 17:25
+ */
+
+namespace app\assets\themes;
+
+use yii\web\AssetBundle;
+
+class SBAdmin2Asset extends AssetBundle
+{
+    public $sourcePath = '@app/assets/themes/startbootstrap-sb-admin-2-gh-pages';
+    public $css = [
+        // MetisMenu CSS
+        'vendor/metisMenu/metisMenu.min.css',
+        //  Custom CSS
+        'dist/css/sb-admin-2.css',
+        // Morris Charts CSS
+        'vendor/morrisjs/morris.css',
+    ];
+    public $js = [
+        // Metis Menu Plugin JavaScript
+        'vendor/metisMenu/metisMenu.min.js',
+        // Morris Charts JavaScript
+        'vendor/raphael/raphael.min.js',
+        'vendor/morrisjs/morris.min.js',
+        'data/morris-data.js',
+        // Custom Theme JavaScript
+        'dist/js/sb-admin-2.js',
+    ];
+    public $depends = [
+        'yii\bootstrap\BootstrapPluginAsset',
+        'rmrevin\yii\fontawesome\AssetBundle',
+    ];
+    public $publishOptions = [
+        'except' => [
+            '*.html',
+        ],
+    ];
+}
