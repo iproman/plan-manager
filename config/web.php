74d5693fbe24a973cd7e0474dda9006886d3fa71
iproman <itechman@yandex.ru>
2019-01-17 00:44:20 +0500

    Remove /web path

--- config/web.php
+++ config/web.php
@@ -13,6 +13,7 @@ $config = [
     ],
     'components' => [
         'request' => [
+            'baseUrl'=> '',
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'SKu92qW2d1lcgtINXiJiIPDee-UslquW',
         ],
