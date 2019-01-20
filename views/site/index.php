f81d6b30ae6ea9ee826daf3fade9911124e3f6d5
iproman <itechman@yandex.ru>
2019-01-20 20:07:50 +0500

    Add chartJS

--- views/site/index.php
+++ views/site/index.php
@@ -4,6 +4,7 @@ use yii\helpers\Url;
 use rmrevin\yii\fontawesome\FAS;
 use app\models\Task;
 use app\components\ViewDetails;
+use dosamigos\chartjs\ChartJs;
 
 /* @var $this yii\web\View */
 /* @var $taskNew \app\models\Task */
@@ -74,7 +75,41 @@ $this->title = 'My Yii Application';
                 </div>
                 <!-- /.panel-heading -->
                 <div class="panel-body">
-                    <div id="morris-area-chart"></div>
+                    <?= ChartJs::widget([
+                        'type' => 'line',
+                        'options' => [
+//                            'height' => 200,
+//                            'width' => 400,
+                        ],
+                        'data' => [
+//                            'radius' =>  "90%",
+                            'labels' => ["January", "February", "March", "April", "May", "June", "July"],
+                            'datasets' => [
+                                [
+                                    'label' => "My First dataset",
+                                    'backgroundColor' => "rgba(179,181,198,0.2)",
+                                    'borderColor' => "rgba(179,181,198,1)",
+                                    'pointBackgroundColor' => "rgba(179,181,198,1)",
+                                    'pointBorderColor' => "#fff",
+                                    'pointHoverBackgroundColor' => "#fff",
+                                    'pointHoverBorderColor' => "rgba(179,181,198,1)",
+                                    'data' => [65, 59, 90, 81, 56, 55, 40]
+                                ],
+                                [
+                                    'label' => "My Second dataset",
+                                    'backgroundColor' => "rgba(255,99,132,0.2)",
+                                    'borderColor' => "rgba(255,99,132,1)",
+                                    'pointBackgroundColor' => "rgba(255,99,132,1)",
+                                    'pointBorderColor' => "#fff",
+                                    'pointHoverBackgroundColor' => "#fff",
+                                    'pointHoverBorderColor' => "rgba(255,99,132,1)",
+                                    'data' => [28, 48, 40, 19, 96, 27, 100]
+                                ],
+
+                            ]
+                        ],
+                    ]);
+                    ?>
                 </div>
                 <!-- /.panel-body -->
             </div>
