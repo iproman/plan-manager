d8d92dde5b0238efab84aaaab09d037dac63d23b
iproman <itechman@yandex.ru>
2019-01-27 12:05:03 +0500

    Make const table, combined 3 migrations

--- migrations/m181205_223818_alter_table_title.php
+++ migrations/m181205_223818_alter_table_title.php
@@ -8,15 +8,28 @@ use app\models\Title;
  */
 class m181205_223818_alter_table_title extends Migration
 {
+
+    const TABLE_NAME = '{{%title}}';
+
     /**
      * {@inheritdoc}
      */
     public function safeUp()
     {
         $this->addColumn(
-            Title::tableName(),
+            self::TABLE_NAME,
             'content',
             $this->text()->null()->comment('Контент')->after('name')
+        );
+        $this->addColumn(
+            self::TABLE_NAME,
+            'branch',
+            $this->string(10)->null()->comment('Ветка')->after('content')
+        );
+        $this->addColumn(
+            self::TABLE_NAME,
+            'status',
+            $this->smallInteger(1)->notNull()->defaultValue(0)->comment('Статус')->after('content')
         );
     }
 
@@ -26,8 +39,16 @@ class m181205_223818_alter_table_title extends Migration
     public function safeDown()
     {
         $this->dropColumn(
-            Title::tableName(),
+            self::TABLE_NAME,
             'content'
+        );
+        $this->dropColumn(
+            self::TABLE_NAME,
+            'branch'
+        );
+        $this->dropColumn(
+            self::TABLE_NAME,
+            'status'
         );
     }
 }
