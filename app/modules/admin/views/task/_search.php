8b8561037073eda634704e6fb2b60c8ccde09b93
iproman <itechman@yandex.ru>
2019-04-09 15:07:09 +0500

    Add task views

---
+++ app/modules/admin/views/task/_search.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use app\models\entities\Task;
+use yii\helpers\Url;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\entities\TaskSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="task-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <div class="row">
+        <div class="col-lg-2"><?= $form->field($model, 'id') ?></div>
+        <div class="col-lg-3"> <?= $form->field($model, 'name') ?></div>
+        <div class="col-lg-2"><?= $form->field($model, 'status')->dropDownList(
+                Task::getStatusNames(),
+                ['prompt' => 'Выберите статус']
+            ) ?>
+        </div>
+        <div class="col-lg-2"><?= $form->field($model, 'branch') ?></div>
+        <div class="col-lg-3"><?= $form->field($model, 'content') ?></div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="form-group">
+                <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+                <?= Html::a(
+                    'Reset',
+                    Url::to(['task/index', 'project_id' => Yii::$app->request->get('project_id')]),
+                    ['class' => 'btn btn-default',]) ?>
+            </div>
+        </div>
+    </div>
+    <?php ActiveForm::end(); ?>
+</div>
---
+++ app/modules/admin/views/task/view.php
@@ -0,0 +1,53 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+use yii\helpers\Url;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\entities\Task */
+
+$this->title = $model->name;
+$this->params['breadcrumbs'][] = ['label' => 'Задачи', 'url' => ['index', 'project_id' => Yii::$app->request->get('project_id')]];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="task-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Обновить', Url::to([
+                'update',
+                'id' => $model->id,
+                'project_id' => Yii::$app->request->get('project_id'),
+            ]
+        ), ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Удалить', Url::to([
+                'delete',
+                'id' => $model->id,
+                'project_id' => Yii::$app->request->get('project_id'),
+            ]
+        ), [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => "Вы уверены, что хотите удалить задачу #{$model->id}?",
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'id',
+            'name',
+            'content',
+            'status',
+            'project_id',
+            'branch',
+            'created_at:datetime',
+            'updated_at:datetime',
+        ],
+    ]) ?>
+
+</div>
