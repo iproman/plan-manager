0c27b0fdebd4ef6959e5d500c7caee53827f9918
iproman <itechman@yandex.ru>
2019-04-23 08:24:00 +0500

    Fix statues on view details widget

--- widgets/views/view-details.php
+++ widgets/views/view-details.php
@@ -1,44 +1,42 @@
 <?php
 
-use app\models\entities\Task;
-use yii\helpers\Html;
+use app\models\service\Statuses;
 
 /** @var $status string */
-/** @var $icon \rmrevin\yii\fontawesome\FAS */
 /** @var $count integer */
 /** @var $text string */
+/** @var $icon string */
 /** @var $link \yii\helpers\Url */
 ?>
 
-<?= Html::tag(
-    'div',
-    Html::tag(
-        'div',
-        Html::tag(
-            'div',
-            Html::tag(
-                'div',
-                '<div class="col-xs-3"><span class="glyphicon ' . $icon . ' gi-5x" aria-hidden="true"></span></div>' .
-                Html::tag(
-                    'div',
-                    Html::tag('div', $count, ['class' => 'huge']) . Html::tag('div', $text),
-                    ['class' => 'col-xs-9 text-right']
-                ),
-                ['class' => 'row']
-            ),
-            ['class' => 'panel-heading']
-        ) . Html::a(
-            Html::tag(
-                'div',
-                Html::tag('span', 'View Details', ['class' => 'pull-left']) .
-                '<span class="glyphicon glyphicon-arrow-right pull-right" aria-hidden="true"></span>' .
-                Html::tag('div', '', ['class' => 'clearfix']),
-                ['class' => 'panel-footer']
-            ),
-            $link),
-        ['class' => 'panel panel-' . Task::getStatusCss()[$status]]
-    ),
-    ['class' => 'col-lg-3 col-md-6']
-)
-?>
+<div class="col-lg-3 col-md-6">
+    <div class="panel panel-<?= Statuses::getStatusCss()[$status] ?>">
+        <div class="panel-heading">
+            <div class="row">
+                <div class="col-xs-3">
+                    <span class="glyphicon <?= $icon ?> gi-5x" aria-hidden="true">
+                    </span>
+                </div>
+                <div class="col-xs-9 text-right">
+                    <div class="huge">
+                        <?= $count ?>
+                    </div>
+                    <div>
+                        <?= $text ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <a href="<?= $link ?>">
+            <div class="panel-footer">
+                <span class="pull-left">
+                    View Details
+                </span>
+                <span class="glyphicon glyphicon-arrow-right pull-right" aria-hidden="true"></span>
+                <div class="clearfix"></div>
+            </div>
+        </a>
+    </div>
+</div>
 
