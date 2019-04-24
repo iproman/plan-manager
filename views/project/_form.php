c707072bb07c45e6089c1469a4ab9a6cf3fdd3c4
iproman <itechman@yandex.ru>
2019-04-24 23:09:21 +0500

    Add fields for status and branch

--- views/project/_form.php
+++ views/project/_form.php
@@ -2,8 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use app\models\entities\Project;
-use app\models\entities\Task;
+use app\models\service\Statuses;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\entities\Project */
@@ -13,13 +12,47 @@ use app\models\entities\Task;
 <div class="plan-form">
 
     <?php $form = ActiveForm::begin(); ?>
+    <div class="row">
+        <div class="col-lg-6 col-sm-12">
+            <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-4">
+            <?= $form->field($model, 'branch')->textInput([
+                'maxlength' => true,
+                'value' => 'iss',
+            ]) ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-4">
+            <?= $form->field($model, 'status', ['labelOptions' => ['style' => 'display: block']])->radioList(
+                Statuses::getStatusLabels(),
+                [
+                    'class' => 'btn-group',
+                    'data-toggle' => 'buttons',
+                    'unselect' => null,
+                    'item' => function ($index, $label, $name, $checked, $value) {
+                        return Html::tag(
+                            'label',
+                            Html::radio($name, $checked, ['value' => $value]) . $label,
+                            [
+                                'class' => 'btn btn-' . Statuses::getStatusCss()[$value] . ($checked ? ' active' : ''),
+                            ]
+                        );
+                    },
+                ]
+            ) ?>
+        </div>
+    </div>
 
-    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+    <hr>
 
     <div class="form-group">
-        <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
-
 </div>
+
