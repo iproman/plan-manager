9ada79cda3ceed170ef9f2ebc072a9ecd95f1264
iproman <itechman@yandex.ru>
2019-05-17 09:18:16 +0500

    Some translation.

--- views/site/index.php
+++ views/site/index.php
@@ -32,7 +32,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
     <div class="row">
         <div class="col-lg-12">
-            <h1 class="page-header">Сегодня <?= date('d-m-Y') ?></h1>
+            <h1 class="page-header">Today is <?= date('d-m-Y') ?></h1>
         </div>
         <!-- /.col-lg-12 -->
     </div>
