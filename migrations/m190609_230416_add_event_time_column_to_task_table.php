c1b622f240109fa8179e4abd1bbf48db1eee09e9
iproman <itechman@yandex.ru>
2019-06-10 04:11:20 +0500

    Add time for tasks.

---
+++ migrations/m190609_230416_add_event_time_column_to_task_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles adding columns to table `{{%task}}`.
+ */
+class m190609_230416_add_event_time_column_to_task_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('{{%task}}', 'event_time', $this->time());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('{{%task}}', 'event_time');
+    }
+}
