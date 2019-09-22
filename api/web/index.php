296d4e1660a2c347c67797e31b56e209facda8e1
iproman <itechman@yandex.ru>
2019-09-22 07:19:38 +0500

    Merge api configs with base.

--- api/web/index.php
+++ api/web/index.php
@@ -6,6 +6,8 @@ require(__DIR__ . '/../../vendor/autoload.php');
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 
 $config = yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/../../config/web.php'),
+    require(__DIR__ . '/../../config/web-local.php'),
     require(__DIR__ . '/../config/main.php'),
     require(__DIR__ . '/../config/main-local.php')
 );
