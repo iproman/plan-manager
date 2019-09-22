aca620b96441b9d637d3b9e23186d48402664992
iproman <itechman@yandex.ru>
2019-09-22 07:18:30 +0500

    Init home test.

---
+++ api/tests/api/HomeCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace api\tests\api;
+
+
+use api\tests\ApiTester;
+
+class HomeCest
+{
+    public function mainPage(ApiTester $I)
+    {
+        $I->sendGET('/');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $I->seeResponseContains('api');
+    }
+}
