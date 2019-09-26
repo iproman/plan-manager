ebf26b8864c1732bb15d87720ba20334c1f64d96
iproman <itechman@yandex.ru>
2019-09-26 12:10:11 +0500

    Fixture task data for tests.

---
+++ api/tests/_data/task.php
@@ -0,0 +1,33 @@
+<?php
+return [
+    [
+        'id' => 1,
+        'name' => 'First Task',
+        'content' => 'First Content',
+        'status' => 1,
+        'branch' => 'iss1',
+        'created_at' => '1392559490',
+        'updated_at' => '1392559490',
+        'project_id' => 1,
+    ],
+    [
+        'id' => 2,
+        'name' => 'Second Task',
+        'content' => 'Second Content',
+        'status' => 2,
+        'branch' => 'iss2',
+        'created_at' => '1392559491',
+        'updated_at' => '1392559491',
+        'project_id' => 1,
+    ],
+    [
+        'id' => 3,
+        'name' => 'Third Task',
+        'content' => 'Third Content',
+        'status' => 3,
+        'branch' => 'iss3',
+        'created_at' => '1392559492',
+        'updated_at' => '1392559492',
+        'project_id' => 1,
+    ],
+];
