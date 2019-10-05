141185038b518f85428c8d4e2506917174a99ceb
iproman <itechman@yandex.ru>
2019-10-05 07:07:42 +0500

    Add admin-layout.

--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -1,12 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
+import {AdminLayoutComponent} from './shared/components/admin-layout/admin-layout.component';
 
 const routes: Routes = [
-
+  {
+    path: '', component: AdminLayoutComponent, children: []
+  }
 ];
 
 @NgModule({
+  declarations: [AdminLayoutComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
